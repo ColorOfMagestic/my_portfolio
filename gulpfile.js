4d8b92a01b21593075ff218bbcba87c209bb0a8e
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-01-08 14:54:05 +0500

    Измениния в gulpfile, добавлены форматы изобр

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass")(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 
-const uncss = require('postcss-uncss');
-const cleanCss = require('gulp-clean-css');
+const uncss = require("postcss-uncss");
+const cleanCss = require("gulp-clean-css");
 
 const autoprefixer = require("autoprefixer");
 const del = require("del");
@@ -20,7 +20,7 @@ const ghPages = require("gulp-gh-pages");
 
 const plugins = [
   uncss({
-    html: ['source/*.html'],
+    html: ["source/*.html"],
     ignore: [
       ".fade",
       ".fade.in",
@@ -29,49 +29,51 @@ const plugins = [
       ".collapsing",
       ".alert-danger",
       ".open",
-      "/open+/"
-    ]
+      "/open+/",
+    ],
   }),
 ];
 
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/style.scss")
+  return gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss(plugins))
-    .pipe(postcss([
-      autoprefixer(),
-      csso()
-    ]))
-    .pipe(cleanCss({
-      level: 0,
-      format: 'keep-breaks'
-    }))
+    .pipe(postcss([autoprefixer(), csso()]))
+    .pipe(
+      cleanCss({
+        level: 0,
+        format: "keep-breaks",
+      })
+    )
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
 // HTML
 
 const html = () => {
-  return gulp.src("source/*.html")
+  return gulp
+    .src("source/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
-}
+};
 
 exports.html = html;
 
 // Scripts
 
 const scripts = () => {
-  return gulp.src("source/js/*.js")
+  return gulp
+    .src("source/js/*.js")
     .pipe(plumber())
     .pipe(concat("main.js"))
     .pipe(gulp.dest("build/js/"))
@@ -79,22 +81,20 @@ const scripts = () => {
     .pipe(concat("main.min.js"))
     .pipe(gulp.dest("build/js/"))
     .pipe(sync.stream());
-}
+};
 
 exports.scripts = scripts;
 
 // Copy
 
 const copy = (done) => {
-  gulp.src([
-    "source/fonts/*.{woff2,woff}",
-    "source/img/**/*.svg",
-  ], {
-    base: "source"
-  })
-    .pipe(gulp.dest("build"))
+  gulp
+    .src(["source/fonts/*.{woff2,woff}", "source/img/**/*.{svg,png,jpg}"], {
+      base: "source",
+    })
+    .pipe(gulp.dest("build"));
   done();
-}
+};
 
 exports.copy = copy;
 
@@ -109,14 +109,14 @@ const clean = () => {
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'build'
+      baseDir: "build",
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
@@ -125,7 +125,7 @@ exports.server = server;
 const reload = (done) => {
   sync.reload();
   done();
-}
+};
 
 // Watcher
 
@@ -133,19 +133,11 @@ const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/js/*.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
-}
+};
 
 // Build
 
-const build = gulp.series(
-  clean,
-  copy,
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-  ),
-);
+const build = gulp.series(clean, copy, gulp.parallel(styles, html, scripts));
 
 exports.build = build;
 
@@ -154,19 +146,12 @@ exports.build = build;
 exports.default = gulp.series(
   clean,
   copy,
-  gulp.parallel(
-    styles,
-    html,
-    scripts,
-  ),
-  gulp.series(
-    server,
-    watcher
-  ));
+  gulp.parallel(styles, html, scripts),
+  gulp.series(server, watcher)
+);
 
-  // deploy
+// deploy
 
-  gulp.task("deploy", function() {
-    return gulp.src("./build/**/*")
-      .pipe(ghPages());
-  });
+gulp.task("deploy", function () {
+  return gulp.src("./build/**/*").pipe(ghPages());
+});
